2f873782cbb3c4d9ba38eb676997766c3cc35148
AlexanederNedelkin <avnedelkin@gmail.com>
2025-12-05 11:04:14 +0200

    AddedChocolateCakeRecipe

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ Dictionary<string, string> recipes = new Dictionary<string, string>()
     { "fried rice", "Cooked rice, Eggs, Vegetables, Soy sauce, Green onions" },
     { "grilled salmon", "Salmon fillet, Olive oil, Lemon, Salt, Pepper" },
     { "chili con carne", "Ground beef, Kidney beans, Tomatoes, Onion, Chili powder" },
+    { "chocolate cake", "Flour, Cocoa powder, Sugar, Eggs, Butter, Baking powder" },
 };
 Console.WriteLine("Enter recipe name to receive its ingredients. Type 'exit' to quit.");
 while (true)
