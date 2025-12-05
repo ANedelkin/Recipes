ad05b238abfd3dc5403d18a28b1af657958a6761
AlexanederNedelkin <avnedelkin@gmail.com>
2025-12-05 10:34:40 +0200

    addedPizzaRecipeAndRecipeGettingLogic

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 
-Dictionary<string, string> recipes = new Dictionary<string, string>();
+Dictionary<string, string> recipes = new Dictionary<string, string>()
+{
+    { "pizza", "Dough, Tomato sauce, Mozzarella, Pepperoni" },
+};
 Console.WriteLine("Enter recipe name to receive its ingredients. Type 'exit' to quit.");
 while (true)
 {
-    string input = Console.ReadLine();
+    string input = Console.ReadLine().ToLower();
     if(input == "exit")
     {
         return;
@@ -12,5 +15,9 @@ while (true)
     if (!recipes.ContainsKey(input))
     {
         Console.WriteLine("No such recipe.");
+    }
+    else
+    {
+        Console.WriteLine(recipes[input]);
     }
 }
